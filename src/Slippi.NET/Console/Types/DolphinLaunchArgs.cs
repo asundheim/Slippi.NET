9b1ff7e5f75922ede99bbf3bb150e7c49de9d88d
Anders Sundheim <andersundheim@gmail.com>
2025-06-15 23:16:25 -0700

    cleanup

--- src/Slippi.NET/Console/Types/DolphinLaunchArgs.cs
+++ src/Slippi.NET/Console/Types/DolphinLaunchArgs.cs
@@ -1,10 +1,5 @@
 ﻿using Newtonsoft.Json;
 using Slippi.NET.Types;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Slippi.NET.Console.Types;
 
